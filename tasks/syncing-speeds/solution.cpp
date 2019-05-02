3a38f6c8aba36df720807737323c7a72e9aeb886
Leo Lee <41759796+smjleo@users.noreply.github.com>
2019-05-02 13:22:40 +0800

    Update solution.cpp

--- tasks/syncing-speeds/solution.cpp
+++ tasks/syncing-speeds/solution.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int x[1e8+2];
+
 int main(){
     int n, r = 1;
     cin >> n;
-    int x[n];
     for (int i = 0; i < n; i++) cin >> x[i];
-    
     for (int i = n-2; i >= 0; i--) 
         r = ((r * x[i+1]) % x[i] ? 
              (r * x[i+1]) / x[i] + 1 : (r * x[i+1]) / x[i]
